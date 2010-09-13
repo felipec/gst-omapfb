55913b6ff9a80bf46980f638d9e818ed3e2442c6
Felipe Contreras <felipe.contreras@gmail.com>
2010-09-13 13:05:53 +0300

    Update copyright
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- omapfb.h
+++ omapfb.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Felipe Contreras <felipe.contreras@gmail.com>
+ * Copyright (C) 2008-2010 Felipe Contreras <felipe.contreras@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
